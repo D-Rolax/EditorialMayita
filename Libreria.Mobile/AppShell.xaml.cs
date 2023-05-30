2eb799e897f0911570ef28b0acabf311763b2559
D-Rolax <78515078+D-Rolax@users.noreply.github.com>
2023-05-29 21:36:32 -0400

    Project architecture

---
+++ Libreria.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Libreria.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Libreria.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Libreria.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
